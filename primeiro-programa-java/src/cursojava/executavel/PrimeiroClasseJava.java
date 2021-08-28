1b0dfc571b8fd96222014b567bd464d26c697719
jhona <jhona@192.168.0.107>
2021-08-28 19:58:20 -0300

    Teste do envio do projeto para o site GitHub

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiroClasseJava {
+	
+	/*Main é um metódo auto executável em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Olá mundo");
+		System.out.println("Este é o meu primeiro código em Java!");
+		
+	}
+}
